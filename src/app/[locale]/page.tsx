a32c7997c30bd59dcb6081f86f3346cb0bf15646
yatochka-dev <philip.chef13@gmail.com>
2024-11-05 19:38:32 +0200

    init v1
    (cherry picked from commit 73b3d3bd505fdd3bb1f50ecfc208073bc02a294c)

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -16,13 +16,10 @@ export default async function Home() {
             />
             <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 flex flex-col">
                 <h1 dangerouslySetInnerHTML={{__html: title}} className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-                    {/*Study Buddy <br/>*/}
-                    {/*Is the way to go {t("title")}*/}
 
                 </h1>
                 <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center self-center">
                     {cap}
-                    {/*Study Buddy is a mobile app that helps you study more efficiently. It's teacher-student collaboration feature allow teachers to give their students material in a better way.*/}
 
                 </p>
             </div>
